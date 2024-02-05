2a087dc3ad4fd4b541168f39b353574bfdb953dd
Famil <familgayibov20@gmail.com>
2024-02-05 15:06:53 -0500

    BREAKING_CHANGE: Add login, signup functionality

--- app.py
+++ app.py
@@ -1,44 +1,104 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, logout_user, login_required, LoginManager, current_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    todos = db.relationship('Todo', backref='user', lazy=True)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(200))
     done = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
-def index():
-    todos = Todo.query.all()
-    return render_template('index.html', todos=todos)
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Username already exists! Please choose a different one.', 'danger')
+        else:
+            new_user = User(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created successfully! Please log in.', 'success')
+            return redirect(url_for('login'))
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('todos'))
+        else:
+            flash('Invalid username or password', 'danger')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Logged out successfully!', 'success')
+    return redirect(url_for('welcome'))
+
+@app.route('/todos')
+@login_required
+def todos():
+    user_todos = Todo.query.filter_by(user_id=current_user.id).all()
+    return render_template('todos.html', todos=user_todos)
 
 @app.route('/add', methods=['POST'])
+@login_required
 def add_todo():
     title = request.form['title']
     description = request.form['description']
-    new_todo = Todo(title=title, description=description)
+    new_todo = Todo(title=title, description=description, user_id=current_user.id)
     db.session.add(new_todo)
     db.session.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for('todos'))
 
 @app.route('/toggle/<int:id>')
+@login_required
 def toggle_done(id):
     todo = Todo.query.get(id)
-    todo.done = not todo.done
-    db.session.commit()
-    return redirect(url_for('index'))
+    if todo.user_id == current_user.id:
+        todo.done = not todo.done
+        db.session.commit()
+    return redirect(url_for('todos'))
 
 @app.route('/delete/<int:id>')
+@login_required
 def delete_todo(id):
-    todo_to_delete = Todo.query.get(id)
-    db.session.delete(todo_to_delete)
-    db.session.commit()
-    return redirect(url_for('index'))
-
+    todo = Todo.query.get(id)
+    if todo.user_id == current_user.id:
+        db.session.delete(todo)
+        db.session.commit()
+    return redirect(url_for('todos'))
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
